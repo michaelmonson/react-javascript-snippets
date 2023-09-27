348a12e8b17cd350d4cd142e238d83424936226c
Michael Monson <michaelmonson@gmail.com>
2023-09-27 15:01:59 -0600

    Update App.js
    Removed unnecessary comments

--- REACT/StateExample/App.js
+++ REACT/StateExample/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 
 import './styles.css';
 
-// don't change the Component name "App"
-// important: In this code editor, use React.useState() instead of just useState()
 export default function App() {
     const [price, setPrice] = React.useState(100);
     
